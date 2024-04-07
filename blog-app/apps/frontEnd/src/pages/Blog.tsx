bbe081091c0c0da3feaf6665b4552a9b9c7c781a
Dwaipayan Mistry <dwaip.mistry@gmail.com>
2024-04-08 00:54:08 +0530

    TailwindCSS changes to blog.tsx

--- blog-app/apps/frontEnd/src/pages/Blog.tsx
+++ blog-app/apps/frontEnd/src/pages/Blog.tsx
@@ -17,8 +17,8 @@ export function Blog() {
     if (blog) {
         return <div >
             <AppBar type="blog" ></AppBar>
-            <div className="  grid   pt-20 mx-24 gap-x-16 md:mx-42 lg:mx-60 xl:mx-80 grid-cols-3">
-                <div id="blog" className="min-w-36  col-span-3 xl:col-span-2">
+            <div className="  grid   pt-20 mx-24 gap-x-16 md:mx-42 lg:mx-50 xl:mx-60 grid-cols-3">
+                <div id="blog" className="min-w-36  col-span-3 lg:col-span-2">
                     <div className="font-extrabold text-4xl">{blog.title}</div>
                     <div className="text-sm font-semibold text-slate-400">
                         Posted on 24-Agu-2024
@@ -27,7 +27,7 @@ export function Blog() {
                         {blog.content}
                     </div>
                 </div>
-                <div className="hidden xl:block">
+                <div className="hidden lg:block">
                     <div id="Author" className=" flex justify-end ">
                         <div>
                             <div className="text-slate-500">
